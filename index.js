f5d446878cecb29417510c0386d34638748c7afb
jamesa <jamesacs329@gmail.com>
2023-08-14 14:40:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyBj56360i7OuhXgK7-PraYGZ9ppAnHJ82Q",
+
+	authDomain: "ascedexchange.firebaseapp.com",
+
+	projectId: "ascedexchange",
+
+	storageBucket: "ascedexchange.appspot.com",
+
+	messagingSenderId: "74402474063",
+
+	appId: "1:74402474063:web:1e579387666d52671da23a",
+};
