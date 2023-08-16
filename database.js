9383c498c9aea5dae68d5617efb5fbb83ef598b5
jamesa <jamesacs329@gmail.com>
2023-08-16 14:47:52 -0400

    day3@2:48pm

--- database.js
+++ database.js
@@ -12,20 +12,6 @@ const firebaseConfig = {
 	appId: "1:74402474063:web:1e579387666d52671da23a",
 };
 
-var uiConfig = {
-	signInSuccessUrl: "<url-to-redirect-to-on-success>",
-	signInOptions: [
-		// Add the sign-in options you want to offer your users
-		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-		firebase.auth.EmailAuthProvider.PROVIDER_ID,
-	],
-	// Add the terms of service and privacy policy URLs
-	tosUrl: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-	privacyPolicyUrl: function () {
-		window.location.assign("https://www.youtube.com/watch?v=xvFZjo5PgG0");
-	},
-};
-
 class textbookListing {
 	constructor(title, edition, price, seller, contact, img) {
 		this.title = title;
@@ -38,5 +24,5 @@ class textbookListing {
 }
 
 firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+
 const db = firebase.firestore();
--- flashcards.js
+++ flashcards.js
@@ -1,4 +1,56 @@
-// make a class
+let sampleData = {
+	"Unit 1": {
+		proton: "positively charged particle",
+		electron: "negatively charged particle",
+		solute: "thing being dissolved in a solution",
+	},
+	description: "Chemistry",
+
+	"Unit 2": {
+		"1 + 1": "2",
+		"2 * 3": "6",
+	},
+	description: "Math",
+};
+
+function updateSlides(cardTitle) {
+	document.querySelector("#title").innerHTML = cardTitle;
+
+	let slideInner = document.querySelector(".carousel-inner");
+	slideInner.innerHTML = "";
+	let set = Object.entries(sampleData[cardTitle]);
+
+	for (let i = 0; i < set.length; i++) {
+		let key = set[i][0];
+		let value = set[i][1];
+
+		// create new term
+		let newTerm = document.createElement("p");
+		newTerm.classList.add("term");
+		newTerm.innerHTML = key;
+
+		// create definition for that term
+		let newDef = document.createElement("p");
+		newDef.classList.add("description");
+		newDef.innerHTML = value;
+
+		// create div to add to slides
+		let newDiv = document.createElement("div");
+		newDiv.classList.add("carousel-item");
+		newDiv.appendChild(newTerm);
+		newDiv.appendChild(newDef);
+
+		// set first term to active by default
+		if (i == 0) {
+			newDiv.classList.add("active");
+		}
+
+		document.querySelector(".carousel-inner").appendChild(newDiv);
+	}
+}
+
+updateSlides("Unit 1");
+
 const carousel = document.querySelector("#carouselExample");
 const cardBody = document.querySelector(".carousel-inner");
 
@@ -18,9 +70,6 @@ function updateCardVariables() {
 }
 
 function flipCard() {
-	console.log("term:", term.style.display);
-	console.log("description:", description.style.display);
-
 	if (window.getComputedStyle(term).display == "block") {
 		term.style.display = "none";
 		description.style.display = "block";
